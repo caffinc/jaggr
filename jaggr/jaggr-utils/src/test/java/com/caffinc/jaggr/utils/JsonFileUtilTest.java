b75cd1a5581d00c4e2e7aa7525e5453cb8230a75
SriramKeerthi <shramk@gmail.com>
2016-11-30 01:01:14 -0800

    Adding support for new aggregations, renaming classes

--- jaggr/jaggr-utils/src/test/java/com/caffinc/jaggr/utils/JsonFileUtilTest.java
+++ jaggr/jaggr-utils/src/test/java/com/caffinc/jaggr/utils/JsonFileUtilTest.java
@@ -12,12 +12,12 @@ import java.nio.file.Paths;
 import java.util.*;
 
 /**
- * Tests the JsonFileReader utility
+ * Tests the JsonFileUtil utility
  *
  * @author Sriram
  * @since 11/27/2016
  */
-public class JsonFileReaderTest {
+public class JsonFileUtilTest {
     private static final String TEMP_DIR = System.getProperty("java.io.tmpdir");
     private static final Random RANDOM = new Random();
     private static final Gson GSON = new Gson();
@@ -32,7 +32,7 @@ public class JsonFileReaderTest {
                 for (int i = 0; i < 10; i++)
                     br.write(i + "\n");
             }
-            lines = JsonFileReader.getFileLines(tempFilePath.toString());
+            lines = JsonFileUtil.getFileLines(tempFilePath.toString());
             for (int i = 0; i < 10; i++) {
                 Assert.assertEquals("Value should match value written to file", String.valueOf(i), lines.get(i));
             }
@@ -56,7 +56,7 @@ public class JsonFileReaderTest {
                     br.write(GSON.toJson(json) + "\n");
                 }
             }
-            List<Map<String, Object>> jsonLines = JsonFileReader.readJsonFromFile(tempFilePath.toString());
+            List<Map<String, Object>> jsonLines = JsonFileUtil.readJsonFromFile(tempFilePath.toString());
             for (int i = 0; i < 10; i++) {
                 Assert.assertEquals("Value should match value written to file", expectedData.get(i), jsonLines.get(i));
             }
@@ -67,7 +67,7 @@ public class JsonFileReaderTest {
 
     @Test
     public void testReadJsonFromResource() throws Exception {
-        List<Map<String, Object>> result = JsonFileReader.readJsonFromResource("raw.json");
+        List<Map<String, Object>> result = JsonFileUtil.readJsonFromResource("raw.json");
         Assert.assertEquals("There must be ten lines in the file", 10, result.size());
         for (Map<String, Object> obj : result) {
             Assert.assertEquals("There must be 9 elements in the object", 9, obj.size());
